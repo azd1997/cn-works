ee79ed37afe4b5b8f668bebd8afa28075589d1d0
azd1997 <374192922@qq.com>
2020-02-07 00:35:06 -0500

    cnworks-go-webserver-doc

--- go/web-server/doc.go
+++ go/web-server/doc.go
@@ -1,3 +1,21 @@
 package web_server
 
 // 简单的文件服务器
+
+// 测试流程
+
+// 1. 前置
+// 在server下创建tmp/123.txt
+// 在client下创建tmp/
+
+// 2. 启动Server
+// cd server
+// go build -o server main.go
+// ./server
+
+// 3. 启动client
+// cd client
+// go build -o client main.go
+// ./client
+
+// 4. 观察输出信息
